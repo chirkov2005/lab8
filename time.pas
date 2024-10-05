80c6e3f191e94de65ab657543e0b7b14b1afd9f3
Булавинова София <bulavinova07s@gmail.com>
2024-10-05 11:08:11 +0300

    More functional

--- time.pas
+++ time.pas
@@ -1,5 +1,7 @@
 ﻿begin
   var year := ReadInteger('Введите год: ');
   
-  Print(((year mod 100) <> 0) and ((year mod 400) <> 0) and ((year mod 4) = 0));
+  var a := ((year mod 100) <> 0) and ((year mod 400) <> 0) and ((year mod 4) = 0);
+  
+  Print(a ? 366 : 365);
 end.
---
+++ timer.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var (h, m, s) := ReadInteger3('Введите данные: ');
+  
+  Print(h * 60 * 60 + m * 60 + s);
+end.
