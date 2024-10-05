eb6297e815bca9fd173015df5f0a48e3670f8996
Булавинова София <bulavinova07s@gmail.com>
2024-10-05 10:52:54 +0300

    Added the new functional to the program

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  Print('Привет, мир!');
+  var hour := ReadInteger('Введите час: ');
+  
+  Assert((hour >= 0) and (hour <= 23));
+  
+  case hour of
+    4..10 : Print('Доброе утро, мир!');
+    11..16 : Print('Добрый день, мир!');
+    17..22 : Print('Добрый вечер, мир!')
+    else Print('Доброй ночи, мир!');
+  end;
 end.
